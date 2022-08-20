25af14718439cb9b6a3aafa0fa7d1326776785f4
CaioOliveiraG2 <caio.silva137@fatec.sp.gov.br>
2022-08-19 22:23:40 -0300

    Atividade Tabata

--- index.ts
+++ index.ts
@@ -2,13 +2,33 @@
 import './style.css';
 
 // Write TypeScript code!
-let title: string = 'MyApp';
+let title: string = 'Tabata';
 document.getElementById('app.title').innerHTML = title;
 
-let counter: number = 0;
+let counter: number = 21;
+let rest: number = 10;
+let ciclos: number = 8;
 
 let intervalId = setInterval(() => {
-  counter = counter + 1;
+  if (counter >= 1 && ciclos >= 8) {
+    counter--;}
+
+  else if (counter <= 1 && ciclos >= 8) {
+    rest--;}
+
+  
+
+  
   let counterDiv = document.getElementById('app.counter');
   counterDiv.innerHTML = counter.toString();
-}, 1000);
+
+  let restDiv = document.getElementById('app.rest');
+  restDiv.innerHTML = rest.toString();
+
+  let ciclosDiv = document.getElementById('app.ciclos');
+  ciclosDiv.innerHTML = ciclos.toString();
+}, 100);
+
+/*{if else (counter >= 19) rest = rest ++;
+  let restDiv = document.getElementById('ap.rest');
+  restDiv.innerHTML = rest.toString();}*/
